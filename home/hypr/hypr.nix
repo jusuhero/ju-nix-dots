a37a40f2d3fe9b69aaa33b838cba4bb79d77642e
Justin Ackermann <justin@ackermann.saarland>
2024-07-31 01:07:18 +0200

    ich will ins bett

--- home/hypr/hypr.nix
+++ home/hypr/hypr.nix
@@ -7,15 +7,16 @@
   ];
 
   wayland.windowManager.hyprland.enable = true;
+  wayland.windowManager.hyprland.package = pkgs.hyprland;
+  wayland.windowManager.hyprland.xwayland.enable = true;
 
   home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    QT_QPA_PLATFORM = "wayland";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-  };
+     NIXOS_OZONE_WL = "1";
+     XDG_CURRENT_DESKTOP = "Hyprland";
+     XDG_SESSION_DESKTOP = "Hyprland";
+     XDG_SESSION_TYPE = "wayland";
+     QT_QPA_PLATFORM = "wayland";
+     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+   };
 
 }
