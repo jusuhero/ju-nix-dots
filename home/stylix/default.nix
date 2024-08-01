04b660f2759d383f638261a70a97a92dff0ae0a2
Justin Ackermann <justin@ackermann.saarland>
2024-08-01 14:42:17 +0200

    soos

--- home/stylix/default.nix
+++ home/stylix/default.nix
@@ -3,9 +3,8 @@
 {
 
   stylix.enable = true;
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
-  stylix.image = ./wallpaper.png;
+  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Modern-Ice";
 
