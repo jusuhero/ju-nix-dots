e9972a7776d62a8bd1c5dccb6797e5d36067e470
Justin Ackermann <justin@ackermann.saarland>
2024-08-01 14:31:35 +0200

    fix

--- home/stylix/default.nix
+++ home/stylix/default.nix
@@ -1,11 +1,11 @@
-{ pkgs, stylix, ... }:
+{ pkgs, ... }:
 
 {
 
   stylix.enable = true;
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
 
-  stylix.image = "../resources/wallpaper.png";
+  stylix.image = "/home/ju/junix-dots/resources/wallpaper.png";
   stylix.cursor.package = pkgs.bibata-cursors;
   stylix.cursor.name = "Bibata-Modern-Ice";
 
